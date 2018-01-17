d824a16113d052d984e757046e5cb9e2fd11b989
MitreFlaviaAntonia <32910419+MitreFlaviaAntonia@users.noreply.github.com>
2018-01-17 20:44:36 +0200

    Add files via upload

---
+++ Lab7SbIpr5.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+struct node *current = NULL;
+
+void print_the_list(struct Node*head)
+{
+    struct node *ptr = head;
+
+    while(ptr != NULL)
+    {
+        printf(" %d ->",ptr->data);
+        ptr = ptr->next;
+    }
+    printf(" NULL");
+
+}
+
+void Sorted_Insert(struct node** head_ref, struct node* new_node)
+{
+    struct node* current;
+
+    if (*head_ref == NULL || (*head_ref)->data >= new_node->data)
+    {
+        new_node->next = *head_ref;
+        *head_ref = new_node;
+    }
+    else
+    {
+        current = *head_ref;
+        while (current->next!=NULL && current->next->data < new_node->data)
+        {
+            current = current->next;
+        }
+        new_node->next = current->next;
+        current->next = new_node;
+    }
+}
+
+struct node *insert_link(int data)
+{
+    struct node *link = (struct node*) malloc(sizeof(struct node));
+    link->data = data;
+    link->next = NULL;
+    return link;
+}
+
+void free_list(struct node* head)
+{
+   struct node* tmp;
+
+   while (head != NULL)
+    {
+       tmp = head;
+       head = head->next;
+       free(tmp);
+    }
+
+}
+int main()
+{
+    struct node *head = NULL;
+    int x;
+    int i;
+    struct node *new_node = NULL;
+    for(i=0;i<=24;i++)
+    {
+        x=rand() % 100 ;
+        printf("\nThe next value in the list is: %d",x);
+        new_node = insert_link(x);
+        Sorted_Insert(&head, new_node);
+    }
+
+    printf("\nThe list after the insertion sort:\n");
+    print_the_list(head);
+    free_list(head);
+    return 0;
+}
